01198e24498e3aaff8388b876c5e6ebeca591e96
Junaid Noor <junaidkhan97991@gmail.com>
2024-05-18 00:17:12 -0700

    Nested loop ques_2

---
+++ nested_loop_exercises/script.js
@@ -0,0 +1,19 @@
+// var years = [2022, 2023];
+// var months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+// var recordsOfEveryMonth = [];
+
+
+// for (let yearsindex = 0; yearsindex < years.length; yearsindex++) {
+//     for (let monthsindex = 0; monthsindex < months.length; monthsindex++) {
+//         for (let daysindex = 0; daysindex < days.length; daysindex++) {
+//             var records = years[yearsindex] + " " + months[monthsindex] + " " + days[daysindex]
+//              recordsOfEveryMonth.push(records)
+            
+//         }
+        
+//     }
+    
+// }
+// console.log(recordsOfEveryMonth);
