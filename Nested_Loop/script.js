81f3b44897868f3223f2aada052c455a97a5c4a2
Junaid Noor <junaidkhan97991@gmail.com>
2024-05-17 23:39:25 -0700

    looping in array part 2

---
+++ Nested_Loop/script.js
@@ -0,0 +1,16 @@
+// var fruits = ["Apple", "Banana", "Mango"];
+// var userFavFruit = prompt("Enter Your Favourite Fruit");
+// var isFruitAvailable = false;
+// for (let i = 0; i < fruits.length; i++) {
+//     if (fruits[i] === userFavFruit) {
+//         isFruitAvailable = true
+//         break
+//     }
+    
+// }
+
+// if(isFruitAvailable === true){
+//     console.log(userFavFruit + " is available");
+// }else{
+//     console.log(userFavFruit + " is not available");
+// }
