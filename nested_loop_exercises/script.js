864d0835f0d89712511c74d8b93b13c81d5819f1
Junaid Noor <junaidkhan97991@gmail.com>
2024-05-18 00:22:53 -0700

    looping in array part_3

--- nested_loop_exercises/script.js
+++ nested_loop_exercises/script.js
@@ -10,10 +10,29 @@
 //         for (let daysindex = 0; daysindex < days.length; daysindex++) {
 //             var records = years[yearsindex] + " " + months[monthsindex] + " " + days[daysindex]
 //              recordsOfEveryMonth.push(records)
-            
+
 //         }
-        
+
 //     }
-    
+
 // }
 // console.log(recordsOfEveryMonth);
+
+
+var fruits = ["APPLE", "MANGO", "BANANA", "GUAVA", "WATERMELON"];
+var userFruit = prompt("Enter a fruit name");
+var isAvailable = false;
+
+for (let i = 0; i < fruits.length; i++) {
+    var searchQuery = userFruit.toLocaleUpperCase()
+    if (fruits[i] === searchQuery) {
+        isAvailable = true;
+        break
+    }
+}
+
+if (isAvailable === true) {
+    console.log("food is available");
+}else{
+    console.log("food is not available");
+}
