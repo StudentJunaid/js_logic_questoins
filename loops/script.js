3250a27abd01d3f100efee65428f44e997a78df9
Junaid Noor <junaidkhan97991@gmail.com>
2024-05-17 23:33:39 -0700

    looping in array

--- loops/script.js
+++ loops/script.js
@@ -4,10 +4,30 @@
 
 // 1st ques
 
-var userNumber = +prompt("Please provide me a table number");
-var tableLength = +prompt("Please provide me a table length");
+// var userNumber = +prompt("Please provide me a table number");
+// var tableLength = +prompt("Please provide me a table length");
 
-for (var i = 1; i <= tableLength; i++) {
-    var tableStatement = userNumber + " x " + i + " = " + (userNumber * i)
-    console.log(tableStatement)
-}
+// for (var i = 1; i <= tableLength; i++) {
+//     var tableStatement = userNumber + " x " + i + " = " + (userNumber * i)
+//     // console.log(tableStatement)
+// }
+
+// 2nd ques
+
+// var cleanCities = ["karachi", "Lahore", "Faisalabad", "Multan"];
+// var userCity = prompt("Enter Your City in Pakistan");
+// var cityFlag = false;
+
+// for (let i = 0; i < cleanCities.length; i++) {
+//     if (userCity === cleanCities[i]) {
+//         cityFlag = true
+//         break
+//     }
+    
+// }
+
+// if (cityFlag === true) {
+//     console.log("cleaned");
+// }else{
+//     console.log("not cleaned");
+// }
