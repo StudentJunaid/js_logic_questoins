a8523c83854950e6224466fc00309755e6a6164a
Junaid Noor <junaidkhan97991@gmail.com>
2024-05-27 08:59:41 -0700

    string questions

--- string length property and slice method with exercises/script.js
+++ string length property and slice method with exercises/script.js
@@ -1,16 +1,41 @@
-var dummyText = "a quick brown fox jumps and over the lazy dog";
- var isavailable = false;
+// var dummyText = "a quick brown fox jumps and over the lazy dog";
+//  var isavailable = false;
 
 
-for (let i = 0; i < dummyText.length; i++) {
-    var checkingSpace = dummyText.slice(i,i + 3)
-    if (checkingSpace === "and") {
-        isavailable = true
+// for (let i = 0; i < dummyText.length; i++) {
+//     var checkingSpace = dummyText.slice(i,i + 3)
+//     if (checkingSpace === "and") {
+//         isavailable = true
+//     }
+// }
+
+// if(isavailable === true){
+//     alert("Your text contain and")
+// }else{
+//     alert("Your text do not contain and")
+// }
+
+// var dummyText = "The New Yorker magazine doesn't allow the phrase.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II World War II World War II World War II"; 
+
+// var count = 0;
+
+// for (let i = 0; i < dummyText.length; i++) {
+//     var checkingtext = dummyText.slice(i,i + 12)
+//     if (checkingtext === "World War II") {
+//         count++
+//     }
+// }
+// alert("your text contaion " + count + " World War II word in sentence")
+
+var dummyText = "The New Yorker magazine doesn't allow the phrase.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II";
+
+for (let index = 0; index < dummyText.length; index++) {
+    var checkingText = dummyText.slice(index, index + 12);
+    if (checkingText === "World War II") {
+        var previousText = dummyText.slice(0, index);
+        console.log("previousText", previousText)
+        dummyText = previousText + " The Second World War"
     }
-}
 
-if(isavailable === true){
-    alert("Your text contain and")
-}else{
-    alert("Your text do not contain and")
 }
+console.log("dummyText", dummyText)
