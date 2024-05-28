534543921c6d51523b8bddb31b7d722a32f2fdea
Junaid Noor <junaidkhan97991@gmail.com>
2024-05-28 08:42:57 -0700

    string finding & replacing methods questions

---
+++ string finding & replacing methods/index.js
@@ -0,0 +1,36 @@
+// var text = "a quick brown a fox jumps over the a lazy dog";
+// var count = 0;
+// for (let index = 0; index < text.length; index++) {
+//     var copyText = text.slice(index, index + 2);
+//      if (copyText === "a ") {
+//         // count = count + 1
+//         count++
+//     }
+// }
+// console.log(count)
+
+
+// var text = "and quick brown and fox jumps over the and lazy dog"; // &
+// for (let index = 0; index < text.length; index++) {
+//     var copyText = text.slice(index, index + 3);
+//     if (copyText === "and") {
+//         var allPreviousText = text.slice(0, index);
+//         var changingWord = "&";
+//         var lastIndex = index + 3;
+//         var nextText = text.slice(lastIndex);
+//         text = allPreviousText + " & " + nextText;
+//     }
+// }
+
+// console.log(text);
+
+// var text = "World War II quick brown World War II fox jumps over the World War II lazy dog"
+// for (var i = 0; i < text.length; i++) {
+//     if (text.slice(i, i + 12) === "World War II") {
+//         text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//     }
+// }
+
+// console.log(text);
+
+// console.log(text.replace(/World War II/g, "the Second World War"))
--- string length property and slice method with exercises/script.js
+++ string length property and slice method with exercises/script.js
@@ -27,15 +27,15 @@
 // }
 // alert("your text contaion " + count + " World War II word in sentence")
 
-var dummyText = "The New Yorker magazine doesn't allow the phrase.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II";
+// var dummyText = "The New Yorker magazine doesn't allow the phrase.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II magazine doesn't allow the phrase World War II.They say it should World War II";
 
-for (let index = 0; index < dummyText.length; index++) {
-    var checkingText = dummyText.slice(index, index + 12);
-    if (checkingText === "World War II") {
-        var previousText = dummyText.slice(0, index);
-        console.log("previousText", previousText)
-        dummyText = previousText + " The Second World War"
-    }
+// for (let index = 0; index < dummyText.length; index++) {
+//     var checkingText = dummyText.slice(index, index + 12);
+//     if (checkingText === "World War II") {
+//         var previousText = dummyText.slice(0, index);
+//         console.log("previousText", previousText)
+//         dummyText = previousText + " The Second World War"
+//     }
 
-}
-console.log("dummyText", dummyText)
+// }
+// console.log("dummyText", dummyText)
